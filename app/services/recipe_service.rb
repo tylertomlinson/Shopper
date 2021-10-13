93736cb6cbe0ce78cf6d9d0aefdd03d8756f8a2f
Tyler Tomlinson <twtomlinson89@gmail.com>
2021-10-12 22:39:06 -0600

    Recipe search returning

--- app/services/recipe_service.rb
+++ app/services/recipe_service.rb
@@ -1,2 +1,11 @@
-class RecipeService
-end 
+class RecipeService < BaseService
+  def recipe_search(keyword)
+    response = conn('https://api.edamam.com').get('/api/recipes/v2') do |f|
+      f.params[:type] = 'public'
+      f.params[:q] = keyword
+      f.params[:app_id] = ENV['application_id']
+      f.params[:app_key] = ENV['application_key']
+    end
+    json(response)
+  end
+end
--- spec/services/recipe_service_spec.rb
+++ spec/services/recipe_service_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe 'recipe service' do
     service = RecipeService.new
     expect(service).to be_instance_of(RecipeService)
   end
+
+  it 'can return search with user inputed keyword' do
+    service = RecipeService.new
+
+    expect(service.recipe_search('chicken')).to be_instance_of(Hash)
+  end
 end
