8ee3dcc5459d09608216e56b4f001fd6fbd414bf
Tyler Tomlinson <twtomlinson89@gmail.com>
2021-10-26 19:27:57 -0600

    Add validations and assocations for recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,12 @@
 class Recipe < ApplicationRecord
+  validates :name, presence: true
+  validates :image, presence: true
+  validates :link, presence: true
+  validates :calories, presence: true
+  validates :sugar, presence: true
+  validates :fat, presence: true
+  validates :type, presence: true
+
+  has_many :user_recipes
+  has_many :users, through: :user_recipes
 end
